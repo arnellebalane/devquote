50588fdac89f2b6bc7470def6ebb74af682d9790
Arnelle Balane <arnellebalane@gmail.com>
2016-04-26 16:21:37 +0800

    proper use of uniquerandomarray; format returned quote as string

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ var path = require('path');
 var uniqueRandomArray = require('unique-random-array');
 
 
+var randomQuote = null;
+
+
 function devquote() {
-    var quotesPath = path.join(__dirname, 'quotes.json');
-    var quotes = JSON.parse(fs.readFileSync(quotesPath, 'utf8'));
-    return uniqueRandomArray(quotes);
+    if (!randomQuote) {
+        var quotesPath = path.join(__dirname, 'quotes.json');
+        var quotes = JSON.parse(fs.readFileSync(quotesPath, 'utf8'));
+        randomQuote = uniqueRandomArray(quotes);
+    }
+    var quote = randomQuote();
+    return quote.text + ' -- ' + quote.author;
 }
 
 
