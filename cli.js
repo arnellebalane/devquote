0dabff904eec12927c90fc4e45e331094cfb7328
Arnelle Balane <arnellebalane@gmail.com>
2016-04-26 17:13:06 +0800

    add cli executable

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var meow = require('meow');
+var devquote = require('./');
+
+
+meow([
+    'Usage:',
+    '  $ devquote'
+]);
+
+
+var quote = devquote();
+console.log('\n  ' + quote + '\n');
